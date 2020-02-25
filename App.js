5ab9172234988b151fd62de482f6c33b72cff895
godswillokokon <godswillokokon3@gmail.com>
2020-02-25 12:43:12 +0100

    theme colors

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { mapping, light as theme } from '@eva-design/eva';
+import { mapping, light as lightTheme } from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './src/navigate/index'
 import 'react-native-gesture-handler'
+import { default as appTheme } from './custom-theme.json';
+
+const theme = { ...lightTheme, ...appTheme };
 
 import SplashScreen from 'react-native-splash-screen';
 SplashScreen.hide();
--- src/screens/pending.js
+++ src/screens/pending.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import {
   Image,
   StyleSheet,
@@ -8,25 +8,87 @@ import {
   Dimensions
 } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
-import { Icon, Layout, Text, Input, TopNavigationAction, TopNavigation, Divider } from '@ui-kitten/components';
+import { Icon, Layout, Text, Input, TopNavigationAction, TopNavigation, Divider, Toggle, } from '@ui-kitten/components';
 
 const BackIcon = (style) => (
-  <Icon {...style} name='arrow-back' />
+  <Icon {...style} name='arrow-ios-back-outline' />
 );
 
 
 export const PendingScreen = ({ navigation }) => {
+  const [available, setAvailable] = useState({
+    checked: false,
+    text: "Not Available"
+  });
+
+
+  const { checked } = available;
+
   const navigateBack = () => {
     navigation.goBack();
   };
   const BackAction = () => (
     <TopNavigationAction icon={BackIcon} onPress={navigateBack} />
   );
+
+
+  const onCheckedChange = (isChecked) => {
+    // console.warn("isChecked", isChecked)
+    setAvailable({ ...available, text: 'Available', checked: isChecked })
+  };
+
+  const availableToggle = () => (
+    <Toggle
+      style={styles.toggle}
+      text={available.text}
+      textStyle={styles.toggleText}
+      onChange={onCheckedChange}
+      checked={checked}
+      size="small"
+    // onPress={this.ToggleAvailability}
+    />
+  );
+
   return (
     <SafeAreaView>
-      <TopNavigation title='CRUTECH Map' leftControl={BackAction()} />
+      <TopNavigation title='Pending Orders' style={styles.topNavigation} titleStyle={styles.title} rightControls={availableToggle()} leftControl={BackAction()} />
       <Divider />
     </SafeAreaView>
   )
 
 };
+
+const styles = StyleSheet.create({
+  topNavigation: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    top: 16,
+    right: 9,
+    marginBottom: 9
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#FD901C',
+    fontWeight: 'bold',
+    // top:
+  },
+  toggle: {
+    margin: 8,
+
+  },
+  toggleText: {
+    fontSize: 12,
+    fontFamily: 'Muli',
+    // alignSelf: 'center',
+    color: '#828282',
+    fontWeight: 'bold',
+    // right: 20
+  }
+});
