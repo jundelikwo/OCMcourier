74546f12c2fcccbec064954c1abedfd15121bb0c
godswillokokon <godswillokokon3@gmail.com>
2020-02-25 16:35:51 +0100

    order toggle set

--- src/screens/pending.js
+++ src/screens/pending.js
@@ -8,7 +8,7 @@ import {
   Dimensions
 } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
-import { Icon, Layout, Text, Input, TopNavigationAction, TopNavigation, Divider, Toggle, Card } from '@ui-kitten/components';
+import { Icon, Layout, Text, Avatar, TopNavigationAction, TopNavigation, Divider, Toggle, Card } from '@ui-kitten/components';
 
 const BackIcon = (style) => (
   <Icon {...style} name='arrow-ios-back-outline' />
@@ -16,14 +16,31 @@ const BackIcon = (style) => (
 
 
 export const PendingScreen = ({ navigation }) => {
+  //driver status
   const [available, setAvailable] = useState({
     checked: false,
     text: "Not Available"
   });
-
-
   const { checked, text } = available;
 
+  const onCheckedChangeCourier = (isChecked) => {
+    // console.warn("isChecked", isChecked)
+    setAvailable({ ...available, text: text == 'Available' ? "Not Available" : "Available", checked: isChecked })
+  };
+
+  //order status
+  const [order, setOrder] = useState({
+    active: false,
+    status: "Not Active"
+  });
+  const { active, status } = order;
+  const onCheckedChangeOrder = (isChecked) => {
+    // console.warn("isChecked", isChecked)
+    setOrder({ ...order, status: status == 'Active' ? "Not Active" : "Active", active: isChecked })
+  };
+
+
+  //nav
   const navigateBack = () => {
     navigation.goBack();
   };
@@ -32,17 +49,13 @@ export const PendingScreen = ({ navigation }) => {
   );
 
 
-  const onCheckedChange = (isChecked) => {
-    // console.warn("isChecked", isChecked)
-    setAvailable({ ...available, text: text == 'Available' ? "Not Available" : "Available", checked: isChecked })
-  };
 
   const availableToggle = () => (
     <Toggle
       style={styles.toggle}
       text={available.text}
       textStyle={styles.toggleText}
-      onChange={onCheckedChange}
+      onChange={onCheckedChangeCourier}
       checked={checked}
       size="tiny"
     // onPress={this.ToggleAvailability}
@@ -50,15 +63,25 @@ export const PendingScreen = ({ navigation }) => {
   );
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.body}>
       <TopNavigation title='Pending Orders' style={styles.topNavigation} titleStyle={styles.title} rightControls={availableToggle()} leftControl={BackAction()} />
       <Divider />
-      <Card>
-        <Text>
-          The Maldives, officially the Republic of Maldives, is a small country in South Asia,
-          located in the Arabian Sea of the Indian Ocean.
-          It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from the Asian continent
-    </Text>
+      {/* style={styles.head} */}
+      <Card >
+        <Avatar style={styles.avatar} size='large' source={require('../assets/person.png')} />
+        <Text style={styles.clientName}> Brown Samson Dappa</Text>
+        <Text style={styles.timeStamp}>2 mins ago</Text>
+        <Text style={styles.kiloTime}>20mins <Text style={styles.dot}>.</Text>2.6km</Text>
+        <Text style={styles.stops}>Stops:<Text style={styles.stopsCount}>3 <Text style={styles.dot}>.</Text></Text>Processed  </Text>
+        <Toggle
+          style={styles.toggleOrder}
+          text={order.status}
+          textStyle={styles.toggleText}
+          onChange={onCheckedChangeOrder}
+          checked={active}
+          size="tiny"
+        // onPress={this.ToggleAvailability}
+        />
       </Card>
     </SafeAreaView>
   )
@@ -77,7 +100,8 @@ const styles = StyleSheet.create({
     top: 16,
     right: 9,
     marginBottom: 9,
-    marginTop: 15
+    marginTop: 15,
+
   },
   title: {
     fontSize: 18,
@@ -98,5 +122,75 @@ const styles = StyleSheet.create({
     color: '#828282',
     fontWeight: 'bold',
     // right: 120
+  },
+  clientName: {
+    fontSize: 16,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#FD901C',
+    fontWeight: 'bold',
+    right: 30,
+    bottom: 52
+  },
+  avatar: {
+    right: 10
+  },
+  timeStamp: {
+    fontSize: 10,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#747D8C',
+    bottom: 70,
+    left: 140
+  },
+  kiloTime: {
+    fontSize: 12,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#747D8C',
+    fontWeight: 'bold',
+    right: 62,
+    bottom: 70
+  },
+  dot: {
+    fontSize: 40,
+    // bottom: 2
+    // position: "absolute",
+    // top: 10
+    // margin: 2
+  },
+  stops: {
+    fontSize: 12,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#747D8C',
+    right: 47,
+    bottom: 74
+    // fontWeight: 'bold',
+  },
+  stopsCount: {
+    fontSize: 12,
+    fontFamily: 'Muli',
+    alignSelf: 'center',
+    color: '#747D8C',
+    fontWeight: 'bold',
+  },
+  head: {
+    height: 90,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+
+  },
+  toggleOrder: {
+    // backgroundColor: '#eeeff1'
+    bottom: 110,
+    left: 100
   }
 });
