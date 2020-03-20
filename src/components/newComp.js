3efa7af1613df493ca9d28dbd9a8428d96bb9eaa
godswillokokon <godswillokokon3@gmail.com>
2020-03-20 15:16:28 +0100

    make comp: boilerplate comp

---
+++ src/components/newComp.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  Text, Avatar, Card,
+} from '@ui-kitten/components';
+
+export default NewComp = () => {
+  return (
+    <View>
+
+    </View>
+  )
+
+};
+
+const styles = StyleSheet.create({})
