14c4c38034ee576e5062f2ea89300916166bca78
godswillokokon <godswillokokon3@gmail.com>
2020-03-23 11:16:30 +0100

    redesigned modal : personal

--- src/screens/presonal.js
+++ src/screens/presonal.js
@@ -13,8 +13,59 @@ import {
 } from '@ui-kitten/components';
 import Icon from 'react-native-vector-icons/Ionicons';
 import TopNav from '../components/topNav';
+import ImagePicker from 'react-native-image-picker';
+
+
+
+
 
 export const ProfileScreen = ({ navigation }) => {
+
+  const [order, setOrder] = useState({
+    filePath: null,
+
+  });
+  // // const { filePath, status } = order;
+  const onChangeFilePath = (isChecked) => {
+    // console.warn("isChecked", isChecked)
+    setOrder({ filePath: isChecked })
+    console.warn("hi");
+
+  };
+
+
+  chooseFile = () => {
+    var options = {
+      title: 'Select Image',
+      customButtons: [
+        { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        let source = response;
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+        this.setState({
+          filePath: source,
+        });
+      }
+    });
+  };
+
 
   const [Namevalue, setValueName] = React.useState('');
   const [Emailvalue, setValueEmail] = React.useState('');
@@ -42,24 +93,65 @@ export const ProfileScreen = ({ navigation }) => {
       setVisible(!visible)
     })
   };
+  // <TouchableOpacity style={{ padding: 14, backgroundColor: '#FD901C', }} onPress={toggleModal}>
+  //   {/* <Icon style={[{ color: '#fff', top: 20 }]} name='ios-arrow-back' size={25} /> */}
+  //   <Text style={{
+  //     fontSize: 18,
+  //     fontFamily: 'Muli',
+  //     // alignSelf: 'center',
+  //     color: '#fff',
+  //     fontWeight: 'bold',
+  //     lineHeight: 23,
+  //     // textAlign: 'center',
+  //     // padding: 5
+  //     // marginBottom: 10
+  //     // bottom: -10
+
+  //   }}>
+
+  //     Back
+
+  //         </Text>
+  // </TouchableOpacity>
   const renderModalElement = () => (
-    <Layout
-      style={styles.modalContainer}>
-      <Image style={{
-        width: 50,
-        height: 50,
-      }} source={require('../assets/success.png')} />
-      <Text style={{
-        fontSize: 18,
-        fontFamily: 'Muli',
-        alignSelf: 'center',
-        color: '#FD901C',
-        fontWeight: 'bold',
-        lineHeight: 23,
-        textAlign: 'center',
-        margin: 20
-      }}>Account Settings Updated</Text>
-      <TouchableOpacity style={{ width: Dimensions.get('window').width - 130, borderRadius: 7, backgroundColor: '#FD901C', padding: 14, alignSelf: 'center', marginTop: 40 }} onPress={toggleModal}>
+    <Layout style={{
+      flex: 1, flexDirection: 'column', height: 290, width: Dimensions.get('window').width - 60, borderRadius: 16, shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 12,
+      },
+      shadowOpacity: 0.3,
+      shadowRadius: 16,
+      elevation: 3,
+
+    }}>
+      <TouchableOpacity style={{ backgroundColor: 'white', flex: 0.6, borderRadius: 16, alignSelf: 'flex-start', width: Dimensions.get('window').width - 60, flexDirection: 'row', justifyContent: 'flex-end' }} onPress={toggleModal}>
+        <Image style={{
+          width: 20,
+          height: 20,
+          margin: 20
+        }} source={require('../assets/x.png')} />
+      </TouchableOpacity>
+      <View style={{ backgroundColor: 'white', flex: 0.6, alignSelf: 'flex-start', width: Dimensions.get('window').width - 60, flexDirection: 'row', justifyContent: 'center' }}>
+        <Image style={{
+          width: 65,
+          height: 65,
+          alignSelf: 'center'
+        }} source={require('../assets/success.png')} />
+      </View>
+      <View style={{ backgroundColor: 'white', flex: 1.5, alignSelf: 'center', width: Dimensions.get('window').width - 60, justifyContent: 'center', margin: 10 }}>
+        <Text style={{
+          fontSize: 18,
+          fontFamily: 'Muli',
+          alignSelf: 'center',
+          color: '#FD901C',
+          fontWeight: 'bold',
+          lineHeight: 23,
+          textAlign: 'center',
+          margin: 20
+        }}>Account Settings Updated</Text>
+      </View>
+      <TouchableOpacity style={{ backgroundColor: '#FD901C', flex: 0.7, alignSelf: 'flex-end', width: Dimensions.get('window').width - 60, justifyContent: 'center', borderBottomRightRadius: 16, borderBottomLeftRadius: 16 }} onPress={toggleModal}>
         <Text style={{
           fontSize: 18,
           fontFamily: 'Muli',
@@ -69,10 +161,10 @@ export const ProfileScreen = ({ navigation }) => {
           lineHeight: 23,
           textAlign: 'center',
         }}>
-          Cancel
-          </Text>
+          Back
+        </Text>
       </TouchableOpacity>
-    </Layout>
+    </Layout >
   );
 
   return (
@@ -91,7 +183,7 @@ export const ProfileScreen = ({ navigation }) => {
             shadowRadius: 2.62,
             elevation: 4,
           }} >
-            <Avatar style={{
+            <Avatar onPress={onChangeFilePath} style={{
               transform: [{
                 scaleX: moderateScale(2, 5)
               }, {
@@ -100,7 +192,13 @@ export const ProfileScreen = ({ navigation }) => {
               alignSelf: 'center',
               borderColor: '#fff',
               // borderWidth: 0.1
+              // backgroundColor: 'red',
+              // flex: 1
             }} size='giant' source={require('../assets/pic.png')} />
+            <TouchableOpacity style={{ width: 20, height: 20, backgroundColor: 'green' }} onPress={onChangeFilePath}></TouchableOpacity>
+
+
+
           </View>
         </View>
         <View style={{ flex: 1.5, paddingTop: 100 }}>
@@ -159,8 +257,8 @@ export const ProfileScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
 
   modalContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     width: Dimensions.get('window').width - 90,
     padding: 16,
     shadowColor: "#000",
@@ -173,7 +271,8 @@ const styles = StyleSheet.create({
     elevation: 3,
     flexDirection: 'column',
     flex: 1,
-    borderRadius: 16
+    borderRadius: 16,
+
   },
 
   inputEmail: {
