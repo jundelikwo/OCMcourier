53a2309f249146039a1ee2748f55a37dcf6f978c
godswillokokon <godswillokokon3@gmail.com>
2020-03-31 18:02:56 +0100

    refac rightBox

--- src/components/rightBox.js
+++ src/components/rightBox.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 import {
   View,
-  Dimensions,
-  ScrollView,
-  ImageBackground,
-  SafeAreaView,
   Text
 } from 'react-native';
 
@@ -13,13 +9,11 @@ import Icon from 'react-native-vector-icons/FontAwesome5';
 export default RightBox = ({ title, status }) => {
   return (
     <View style={{ position: 'absolute', backgroundColor: 'transparent', left: 55, bottom: -10 }}>
-
       <View style={{
         width: 100,
         height: 60,
         backgroundColor: '#dcd6f7',
         borderRadius: 10,
-
       }} />
       <Text style={{
         position: 'absolute',
@@ -29,27 +23,20 @@ export default RightBox = ({ title, status }) => {
         left: 17,
         top: 5,
         color: '#515C6F',
-        // backgroundColor: 'green',
         width: 70,
         fontWeight: 'bold',
-        // padding: 10
         height: 34.5
-
       }}>{title}</Text>
       <Text style={{
         position: 'absolute',
-
         zIndex: 2,
         fontSize: 11,
         left: 17,
         top: 40,
         color: '#515C6F',
-        // backgroundColor: 'red',
         width: 60,
-
-
       }}>{status}</Text>
-      {/* <Icon name='ello' style={{ position: 'relative', alignSelf: 'flex-end', left: 33, bottom: 16 }} size={8} color={'#5AC966'} /> */}
+      <Icon name='ello' style={{ position: 'absolute', alignSelf: 'flex-end', left: 72, bottom: 7 }} size={8} color={'#5AC966'} />
       <View style={{
         position: 'absolute',
         left: -15,
@@ -62,10 +49,7 @@ export default RightBox = ({ title, status }) => {
         borderRightColor: '#dcd6f7',
         borderBottomWidth: 13,
         borderBottomColor: 'transparent',
-
       }} />
-
     </View>
-
   )
 };
