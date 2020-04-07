f7ef9c60a21a089e4008352e04db91dc85665b36
godswillokokon <godswillokokon3@gmail.com>
2020-04-07 18:01:44 +0100

    set data set and divided deliver data from orders

--- src/screens/pending.js
+++ src/screens/pending.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   View,
   ScrollView,
@@ -10,8 +10,58 @@ import {
 import TopNav from '../components/topNav';
 import OrderStatus from '../components/orderStatus';
 import PendingCards from '../components/pendingCards';
+import PendingBuyer from '../components/pendingBuyer';
 
-export const PendingScreen = () => {
+export const PendingScreen = ({ navigation }) => {
+  const [buyer, setBuyer] = useState(
+    {
+      'title': 'Godswill Effiong Okokon',
+      'status': false,
+      'distance': '5km ',
+      'address': '15 Harcourt Street, Calabar.',
+      'phone': '+2348177024847'
+    }
+  );
+
+  const [orders, setOrders] = useState([
+    {
+      'orderCount': 1,
+      'title': 'Crunchies Restaurant',
+      'status': true,
+      'distance': '1km ',
+      'time': '05mins',
+      'address': '15 Harcourt Street, Calabar.',
+      'phone': '+23409256345'
+
+    },
+    {
+      'orderCount': 2,
+      'title': 'The Spot',
+      'status': false,
+      'distance': '1.5km ',
+      'time': '12mins',
+      'address': '36 Abasi Edem Street, Calabar.',
+      'phone': '+23412345678'
+    },
+    {
+      'orderCount': 3,
+      'title': 'Pepper Roni',
+      'status': false,
+      'distance': '1.3km ',
+      'time': '10mins',
+      'address': '10 State Housing, Calabar.',
+      'phone': '+234098765432'
+    },
+    {
+      'orderCount': 4,
+      'title': 'Happy Food',
+      'status': false,
+      'distance': '2.1km ',
+      'time': '15mins',
+      'address': ' 6A, Housing Estate Road (Beside Hennyplan winery), opposite First Bank, Off Marian Road, Calabar.',
+      'phone': '+238177024847'
+    }
+  ]);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNav title='Pending Orders' />
@@ -19,7 +69,14 @@ export const PendingScreen = () => {
       <View style={{ flex: 1 }}>
         <OrderStatus />
         <ScrollView >
-          <PendingCards />
+          {
+            orders.map((order, i) => {
+              return (
+                <PendingCards navigation={navigation} buyer={buyer} order={order} index={i} key={i} />
+              )
+            })
+          }
+          <PendingBuyer buyer={buyer} />
         </ScrollView>
       </View>
     </SafeAreaView>
