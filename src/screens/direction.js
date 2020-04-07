1e45b1eb476e885edd494d0c993dc4b319be3247
godswillokokon <godswillokokon3@gmail.com>
2020-04-07 12:25:02 +0100

    goddddddd i just made this map work

--- src/screens/direction.js
+++ src/screens/direction.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   View,
   Dimensions,
-  SafeAreaView
+  SafeAreaView,
+  Text
 } from 'react-native';
 import {
   Divider,
@@ -24,6 +25,11 @@ let ACCURACY;
 const LATITUDE_DELTA = 0.046;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+let initLATITUDE = 4.975715;
+let initLONGITUDE = 8.3417;
+let initLATITUDE_DELTA = 0.046;
+let initLONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 GetLocation.getCurrentPosition({
   enableHighAccuracy: true,
   timeout: 15000,
@@ -31,10 +37,10 @@ GetLocation.getCurrentPosition({
   .then(coords => {
     LATITUDE = coords.latitude;
     LONGITUDE = coords.longitude;
-    // TIME = coords.time;
-    // BEARING = coords.bearing;
-    // SPEED = coords.speed;
-    // ACCURACY = coords.accuracy;
+    TIME = coords.time;
+    BEARING = coords.bearing;
+    SPEED = coords.speed;
+    ACCURACY = coords.accuracy;
 
   })
   .catch(error => {
@@ -44,7 +50,11 @@ GetLocation.getCurrentPosition({
 
 
 export const DirectionScreen = ({ navigation }) => {
-  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+
+    // console.log('huu')
+
+  }, [])
 
   var mapStyle = [{ "elementType": "geometry", "stylers": [{ "color": "#242f3e" }] }, { "elementType": "labels.text.fill", "stylers": [{ "color": "#746855" }] }, { "elementType": "labels.text.stroke", "stylers": [{ "color": "#242f3e" }] }, { "featureType": "administrative.locality", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#263c3f" }] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [{ "color": "#6b9a76" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#38414e" }] }, { "featureType": "road", "elementType": "geometry.stroke", "stylers": [{ "color": "#212a37" }] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [{ "color": "#9ca5b3" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#746855" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#1f2835" }] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [{ "color": "#f3d19c" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#2f3948" }] }, { "featureType": "transit.station", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#17263c" }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "color": "#515c6d" }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "color": "#17263c" }] }];
 
@@ -55,31 +65,43 @@ export const DirectionScreen = ({ navigation }) => {
       <TopNav title='Get Direction' />
       <Divider />
       <View style={{ flex: 1 }}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={{ height: Dimensions.get('window').height - 100, width: Dimensions.get('window').width }}
-          region={{
-            latitude: LATITUDE,
-            longitude: LONGITUDE,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-            // time: TIME,
-            // bearing: BEARING,
-            // speed: SPEED,
-            // accuracy: ACCURACY
-          }}
-          customMapStyle={mapStyle}
-          // mapType={'hybrid'}
-          showsUserLocation={true}
-          userLocationPriority={'high'}
-          userLocationFastestInterval={4000}
-          userLocationUpdateInterval={5000}
-          showsIndoorLevelPicker={true}
-          loadingEnabled={true}
-          loadingIndicatorColor={'#606060'}
-          loadingBackgroundColor={'#FFF'}
-          isAccessibilityElement={true}
-        />
+        {/* {console.log("LATITUDE :", LATITUDE, "LONGITUDE :", LONGITUDE, "LATITUDE_DELTA", LATITUDE_DELTA, "LONGITUDE_DELTA", LONGITUDE_DELTA)} */}
+        {console.log(LATITUDE)}
+        {LATITUDE ?
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={{ height: Dimensions.get('window').height - 100, width: Dimensions.get('window').width }}
+            initialRegion={{
+              latitude: initLATITUDE,
+              longitude: initLONGITUDE,
+              latitudeDelta: initLATITUDE_DELTA,
+              longitudeDelta: initLONGITUDE_DELTA,
+            }}
+            region={{
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+              time: TIME,
+              bearing: BEARING,
+              speed: SPEED,
+              accuracy: ACCURACY
+            }}
+            customMapStyle={mapStyle}
+            // mapType={'hybrid'}
+            showsUserLocation={true}
+            userLocationPriority={'high'}
+            userLocationFastestInterval={4000}
+            userLocationUpdateInterval={5000}
+            showsIndoorLevelPicker={true}
+            loadingEnabled={true}
+            loadingIndicatorColor={'#606060'}
+            loadingBackgroundColor={'#FFF'}
+            isAccessibilityElement={true}
+            showsBuildings={true}
+            loadingEnabled={true}
+          />
+          : <View style={{ flex: 1, justifyContent: 'center' }}><Text style={{ alignSelf: 'center', fontSize: 15 }}>Check Internet Connection</Text></View>}
       </View>
     </SafeAreaView >
   )
