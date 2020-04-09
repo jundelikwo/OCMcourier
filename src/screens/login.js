99d0e666bbb8fdf1d80adf79f1975d17306109ff
godswillokokon <godswillokokon3@gmail.com>
2020-04-09 17:10:15 +0100

    clean: login screen

--- src/screens/login.js
+++ src/screens/login.js
@@ -33,24 +33,17 @@ export const LoginScreen = ({ navigation }) => {
     requestAnimationFrame(() => {
       navigation.navigate('Pending');
     })
-
   };
   const [Evalue, setValueE] = React.useState('');
   const [Pvalue, setValueP] = React.useState('');
 
-
   return (
-
     <SafeAreaView style={styles.container}>
-
       <LinearGradient colors={['#D86800', '#d97309', '#e77307', '#EA7D00', '#ed8304', '#ed8302', '#ed8200',]} style={styles.gradient}>
-
         <Text style={styles.loginText}>Login</Text>
         <Image source={require('../assets/loginVector.png')} style={styles.img} />
         <KeyboardAvoidingView style={styles.key} behavior="padding" enabled>
-
           <Layout style={styles.form}>
-
             <Input
               value={Evalue}
               placeholder='Email'
@@ -77,26 +70,18 @@ export const LoginScreen = ({ navigation }) => {
               textStyle={styles.placeholder}
               placeholderTextColor={'#828282'}
             />
-
           </Layout>
           <TouchableOpacity style={styles.forgotBut}><Text style={styles.forgot}>Forgot password?</Text></TouchableOpacity>
-
           <TouchableOpacity style={styles.button} onPress={navigatePending}><Text style={styles.buttonText}>Login</Text></TouchableOpacity>
         </KeyboardAvoidingView>
-
-
-
       </LinearGradient>
-
     </SafeAreaView >
-
   )
-
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
   },
   gradient: {
     flex: 1,
@@ -146,18 +131,13 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width / 2.7,
     top: 40,
     alignSelf: 'flex-start',
-    // alignItems: 'center',
-    // justifyContent: 'flex-end'
   },
   forgot: {
     fontSize: 13,
     fontFamily: 'Muli',
     color: '#ffffff',
     backgroundColor: 'transparent',
-    // alignSelf: 'center',
     marginLeft: 20
-
-
   },
   button: {
     backgroundColor: '#fff',
@@ -165,10 +145,8 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderRadius: 15,
     padding: 12,
-    // marginVertical: 10,
     marginTop: 60,
     top: 60
-    // top: Dimensions.get('window').height - 750
   },
   buttonText: {
     fontSize: 18,
