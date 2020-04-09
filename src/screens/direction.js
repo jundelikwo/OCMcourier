00ce367fd59fca105c5fc8048860bbb80f0d4873
godswillokokon <godswillokokon3@gmail.com>
2020-04-09 17:07:53 +0100

    clean: direction screen

--- src/screens/direction.js
+++ src/screens/direction.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   View,
   Dimensions,
@@ -11,8 +11,6 @@ import {
 import TopNav from '../components/topNav';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import GetLocation from 'react-native-get-location';
-
-
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -41,19 +39,14 @@ GetLocation.getCurrentPosition({
     BEARING = coords.bearing;
     SPEED = coords.speed;
     ACCURACY = coords.accuracy;
-
   })
   .catch(error => {
     const { code, message } = error;
-    //  console.warn(code, message);
   })
-
 
 export const DirectionScreen = ({ navigation }) => {
 
   var mapStyle = [{ "elementType": "geometry", "stylers": [{ "color": "#242f3e" }] }, { "elementType": "labels.text.fill", "stylers": [{ "color": "#746855" }] }, { "elementType": "labels.text.stroke", "stylers": [{ "color": "#242f3e" }] }, { "featureType": "administrative.locality", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#263c3f" }] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [{ "color": "#6b9a76" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#38414e" }] }, { "featureType": "road", "elementType": "geometry.stroke", "stylers": [{ "color": "#212a37" }] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [{ "color": "#9ca5b3" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#746855" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#1f2835" }] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [{ "color": "#f3d19c" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#2f3948" }] }, { "featureType": "transit.station", "elementType": "labels.text.fill", "stylers": [{ "color": "#d59563" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#17263c" }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "color": "#515c6d" }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "color": "#17263c" }] }];
-
-
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -81,7 +74,6 @@ export const DirectionScreen = ({ navigation }) => {
               accuracy: ACCURACY
             }}
             customMapStyle={mapStyle}
-            // mapType={'hybrid'}
             showsUserLocation={true}
             userLocationPriority={'high'}
             userLocationFastestInterval={4000}
@@ -94,13 +86,13 @@ export const DirectionScreen = ({ navigation }) => {
             showsBuildings={true}
             loadingEnabled={true}
             loadingIndicatorColor={'#FD901C'}
-
           />
           : <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text style={{ alignSelf: 'center', fontSize: 30, textAlign: 'center', margin: 10 }}>Can't get device location. Please check your network or location settings!</Text>
+            <Text style={{ alignSelf: 'center', fontSize: 30, textAlign: 'center', margin: 10 }}>
+              Can't get device location. Please check your network or location settings!
+            </Text>
           </View>}
       </View>
     </SafeAreaView >
   )
 };
-
